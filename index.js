d27ee4eaedb4d8387153ea550705486dc7c5c942
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-11-28 22:56:59 +0530

    refresh

---
+++ index.js
@@ -0,0 +1,14 @@
+// Functions - A section of reusable code , 
+//             declare code once , use it whenever you want
+//         calll the function to execute that code
+
+
+
+function happyBirthday(){
+    console.log("happy birthafy to you");
+    console.log("happy birthafy to you");
+    console.log("happy birthafy to you");
+    console.log("happy birthafy to you");
+    
+}
+happyBirthday();
